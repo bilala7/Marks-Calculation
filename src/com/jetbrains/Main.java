41282ca675be0a7f0b0a9e6447faf24e7126c4b2
bilal <bilalazmy7@gmail.com>
2021-11-28 11:58:05 +0530

    updated

---
+++ src/com/jetbrains/Main.java
@@ -0,0 +1,20 @@
+package com.jetbrains;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int mk01 = 78;
+        int mk02 = 58;
+        int mk03 = 99;
+        int total = mk01 + mk02 + mk03;
+
+        System.out.println("Marks 01 : "+mk01);
+        System.out.println("Marks 02 : "+mk02);
+        System.out.println("Marks 03 : "+mk03);
+        System.out.println("Total : "+total);
+
+    }
+
+    }
